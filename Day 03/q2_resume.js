5c30b91943113dcb655310da5f31676305c65663
kiranbaby14 <kiranbaby256@gmail.com>
2021-12-09 18:32:06 +0530

    Day 03

---
+++ Day 03/q2_resume.js
@@ -0,0 +1,13 @@
+let resume = {
+    "fullName": "Kiran Baby",
+    "qualification": "B-Tech",
+    "pgmLanguages": ["Javascript", "Python", "C", "C#"],
+    "techStack": ["MERN", "Django"],
+    "degreeScore": "8.09",
+    "projects": ["Multiplayer TPS game", "auto webpage creator"],
+    "awards": ["Game Begetter", "CSI-Inapp"],
+    "tools": ["VScode", "Pycharm", "Git", "Github", "Unity", "Android Studio"],
+    "languages": ["Malayalam", "English", "Hindi"],
+    "hobbies": ["Music", "COding", "Football", "Cricket", "Video Games"],
+    "addQualification": {"X": "8.2", "XII": "8.2"}
+}

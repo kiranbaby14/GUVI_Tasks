3a057c85b8cd85a9a1762775b9c043dda0088510
kiranbaby14 <kiranbaby256@gmail.com>
2021-12-10 13:25:49 +0530

    Day 03

--- Day 03/JS_JSON_Files/q2_resume.js
+++ Day 03/JS_JSON_Files/q2_resume.js
@@ -1,13 +1,32 @@
 let resume = {
-    "fullName": "Kiran Baby",
-    "qualification": "B-Tech",
+    "personalInfo": {
+        "fullName": "Kiran Baby",
+        "emailID": "kiranbaby256@gmail.com",
+        "github": "https://github.com/kiranbaby14",
+        "linkedin": "https://www.linkedin.com/in/kiranbaby14/",
+        "phoneNumber": "7510207694",
+    },
+
+    "qualification": {
+        "degree": "B_Tech",
+        "score": "8.09 CGPA",
+        "university": "APJ Abdul Kalam University",
+        "branch": "CSE"
+    },
+
+    "additionalQualification":{
+        "X": "8.2 CGPA", 
+        "XII": "8.2 CGPA"
+    },
+
     "pgmLanguages": ["Javascript", "Python", "C", "C#"],
-    "techStack": ["MERN", "Django"],
-    "degreeScore": "8.09",
-    "projects": ["Multiplayer TPS game", "auto webpage creator"],
+    "techStacks": ["MERN", "Django"],
+    "projects": ["Hyperface implemented Multiplayer TPS game", "auto webpage creator", "Ninjesk-Game using Unity"],
     "awards": ["Game Begetter", "CSI-Inapp"],
-    "tools": ["VScode", "Pycharm", "Git", "Github", "Unity", "Android Studio"],
+    "tools": ["VScode", "Pycharm", "Git", "Github", "Unity", "AndroidStudio"],
     "languages": ["Malayalam", "English", "Hindi"],
     "hobbies": ["Music", "COding", "Football", "Cricket", "Video Games"],
-    "addQualification": {"X": "8.2", "XII": "8.2"}
+    "strengths": ["Dedication", "Hardworking", "Leadership"]
 }
+
+
